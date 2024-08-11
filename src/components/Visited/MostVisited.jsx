4e14f40d61e438427bb7f219048d6a51e605c43d
lamidimorufat <lamidimorufat0@gmail.com>
2024-08-11 17:49:25 +0100

    fix: worked on the most visited responsiveness

--- src/components/Visited/MostVisited.jsx
+++ src/components/Visited/MostVisited.jsx
@@ -1,31 +1,40 @@
 import React from 'react'
 import { VisitedData } from './VisitedData'
-import MostVisitedCard from '../ReUsable/MostVisitedCard'
-import Button from '../ReUsable/Button'
+import { FaStar } from 'react-icons/fa'
+
 
 const MostVisited = ({id}) => {
   return (
   <section id={id} className='py-4 lg:py-10 mt-10 md:mt-[6rem] px-4 lg:px-20'>
         <div className=''>
         <h2 className='text-xl md:text-3xl font-bold text-left md:text-center mb-8'>Most Visited Destinations</h2>
-        <div className='flex flex-col lg:flex-row p-4 lg:space-x-10 items-center '>
-            {VisitedData.map((item, index) => (
-                <MostVisitedCard
-                key={index}
-                image={item.image}
-                imageTitle={item.imageTitle}
-                title={item.title}
-                locationArrow ={item.locationArrow} 
-                location={item.location}
-                description={item.description}
-                fee={item.fee} 
-                rating={item.rating}
-                button={ <Button className="w-full md:w-[30%]">
-                Book Now
-              </Button>}
-            />
-            ))}
-        </div>
+        <div className='py-5 lg:py-20 px-4 lg:px-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
+                {VisitedData.map((data, index) => (
+                    <div key={index} className='bg-white py-5 px-5 border shadow-lg rounded-md'>
+                        <img src={data.image} alt={data.imageTitle} className='w-full object-cover rounded-md  transform transition duration-500 hover:scale-105 ' />
+                        <div className='flex justify-between items-center'>
+                           <h3 className='text-lg font-semibold mt-3'>{data.title}</h3>
+                            <div className="flex items-center mb-2">
+                              {Array(data.rating).fill().map((_, i) => (
+                              <FaStar key={i} className="text-yellow-500" />
+                                ))}
+                            </div>
+                          
+                        </div>
+                        <div className='flex items-center gap-4'>
+                          <p className=''>{data.locationArrow}</p>
+                        <p className=''>{data.location}</p>
+                        </div>
+                       
+                        <p className='mt-2'>{data.description}</p>
+                      
+                        <h3 className='text-lg font-semibold mt-3'>{data.fee}</h3>
+                      
+                        
+                        <a href={`/booking/${data.id}`} className='mt-4 inline-block bg-blue text-white px-4 py-2 rounded-md'>Book Now</a>
+                    </div>
+                ))}
+            </div>
         </div>
       
     </section>
--- src/components/Visited/VisitedData.jsx
+++ src/components/Visited/VisitedData.jsx
@@ -2,16 +2,6 @@ import { FaMapPin } from "react-icons/fa";
 
 export const VisitedData = [
     {
-        image: "https://res.cloudinary.com/drphumgcb/image/upload/v1722264747/pexels-pixabay-161401_1_ijza2o.jpg",
-        imageTitle: "Kyoto Japan",
-        title: "Kyoto",
-        rating: 5,
-        locationArrow: <FaMapPin/>,
-        location: "Japan",
-        description: "Experience Kyoto's blend of ancient temples, tea houses, and cherry blossom gardens.",
-        fee: "$1200"
-    },
-    {
         image: "https://res.cloudinary.com/drphumgcb/image/upload/v1722265023/pexels-pixabay-163864_hdndxb.jpg",
         imageTitle: "Santorini, Greece",
         title: "Santorini",
@@ -20,6 +10,16 @@ export const VisitedData = [
         location: " Greece",
         description: "Experience Santorini's stunning sunsets, white-washed buildings, and crystal-clear waters.",
         fee: "$700"
+    },
+    {
+        image: "https://res.cloudinary.com/drphumgcb/image/upload/v1722264747/pexels-pixabay-161401_1_ijza2o.jpg",
+        imageTitle: "Kyoto Japan",
+        title: "Kyoto",
+        rating: 5,
+        locationArrow: <FaMapPin/>,
+        location: "Japan",
+        description: "Experience Kyoto's blend of ancient temples, tea houses, and cherry blossom gardens.",
+        fee: "$1200"
     },
     {
         image: "https://res.cloudinary.com/drphumgcb/image/upload/v1722265693/pexels-viviana-ceballos-1480481-3341574_cuzmww.jpg",
